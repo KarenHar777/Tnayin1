360b3aa85a719820df0f720e14fe5cb5378c1fd2
KarenHarutyunyan <karenvardanovich@gmail.com>
2023-01-07 17:47:15 +0400

    Add project files.

---
+++ NewClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassHomework
+{
+    public class NewClass
+    {
+        public string name;
+
+        public int age;
+        private string v;
+
+        public NewClass()
+        {
+            age = 1;
+            name= "Mukik";
+        }
+
+        public NewClass(string n)
+        {
+            age = 31;
+            name = n;
+        }
+
+        public NewClass(int a, string n)
+        {
+            age = a;
+            name = n;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{age} {name}");
+        }
+
+        public int GetAge(int a)
+        {
+            int resoult =a-15;
+            return resoult;
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassHomework;
+
+public  class Program
+{
+        static void Main(string[] args)
+    {
+
+        NewClass class1= new NewClass();
+        class1.Print();
+
+        NewClass class2 = new NewClass("Lana");
+        class2.Print();
+
+        NewClass class3 = new NewClass(27, "Inna");
+        class3.Print();
+
+        int x = class1.GetAge(38);
+
+        Console.WriteLine(x);
+    }
+
+    
+
+
+    
+
+}
